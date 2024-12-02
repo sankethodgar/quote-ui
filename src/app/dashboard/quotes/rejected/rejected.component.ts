4f7f4306909bca6d52641b6981186a58fc0ea193
aniers4 <sankethodgar04@gmail.com>
2024-12-02 18:23:54 +0530

    feat: add pagination navigation to quotes components

--- src/app/dashboard/quotes/rejected/rejected.component.ts
+++ src/app/dashboard/quotes/rejected/rejected.component.ts
@@ -115,6 +115,75 @@ import { Component } from '@angular/core'
         </div>
       </div>
     </div>
+    <nav
+      class="flex items-center gap-x-1 mt-5 justify-end"
+      aria-label="Pagination"
+    >
+      <button
+        type="button"
+        class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+        aria-label="Previous"
+        disabled=""
+      >
+        <svg
+          class="shrink-0 size-3.5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
+        <span>Previous</span>
+      </button>
+      <div class="flex items-center gap-x-.5">
+        <button
+          type="button"
+          class="flex justify-center items-center bg-blue-500 text-white py-2 px-3 text-sm rounded-lg"
+          aria-current="page"
+        >
+          1
+        </button>
+        <button
+          type="button"
+          class="flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg"
+        >
+          2
+        </button>
+        <button
+          type="button"
+          class="flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg"
+        >
+          3
+        </button>
+      </div>
+      <button
+        type="button"
+        class="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800"
+        aria-label="Next"
+      >
+        <span>Next</span>
+        <svg
+          class="shrink-0 size-3.5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
+      </button>
+    </nav>
   `,
 })
 export class RejectedComponent {
