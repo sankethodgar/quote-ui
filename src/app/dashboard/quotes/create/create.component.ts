4be7767bed59e1cde4810cdb42bb8eb1dd0d75f3
aniers4 <sankethodgar04@gmail.com>
2024-12-03 15:39:06 +0530

    feat: enhance quote creation component with improved navigation and UI updates

--- src/app/dashboard/quotes/create/create.component.ts
+++ src/app/dashboard/quotes/create/create.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
 })
-export class CreateComponent {}
+export class CreateComponent {
+  constructor(private router: Router) {}
+
+  reload() {
+    const currentUrl = this.router.url
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl])
+    })
+  }
+}
