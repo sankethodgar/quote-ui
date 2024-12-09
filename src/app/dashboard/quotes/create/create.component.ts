d978936f72debb82f75c6a1ae51612bce61f1092
aniers4 <sankethodgar04@gmail.com>
2024-12-09 11:40:37 +0530

    feat: enhance quote creation component with radio button selections and submission functionality

--- src/app/dashboard/quotes/create/create.component.ts
+++ src/app/dashboard/quotes/create/create.component.ts
@@ -20,6 +20,13 @@ export class CreateComponent implements OnInit {
   employee: any = []
   materials: any = []
 
+  reload() {
+    const currentUrl = this.router.url
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl])
+    })
+  }
+
   ngOnInit() {
     this.http.get(`${environment.apiUrl}/customers`).subscribe((data) => {
       this.customers = data
@@ -44,17 +51,13 @@ export class CreateComponent implements OnInit {
   deliveryDate: string = ''
   validFromDate: string = ''
   validToDate: string = ''
+  selectedDescription: string = ''
   salesEmployee: any = ''
 
   onCustomerSelect() {
     this.selectedCustomer = this.customers.find(
       (customer: any) => customer.customernumber === this.selectedCustomer
     )
-    // if (this.selectedCustomer) {
-    //   const randomEmployee =
-    //     this.employee[Math.floor(Math.random() * this.employee.length)]
-    //   this.salesEmployee = randomEmployee.employeeno
-    // }
   }
 
   onShipToSelect() {
@@ -69,10 +72,34 @@ export class CreateComponent implements OnInit {
     )
   }
 
-  reload() {
-    const currentUrl = this.router.url
-    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-      this.router.navigate([currentUrl])
+  onStep1Submit() {
+    const quoteData = {
+      CustomerNo: this.selectedCustomer.customernumber,
+      customerEnquiryNo: this.customerEnquiryNo,
+      CustomerName1: this.selectedCustomer.name,
+      CustomerName2: this.selectedCustomer.name2,
+      ShipToNo: this.shipToCustomer.customernumber,
+      ShipToCustomerName1: this.shipToCustomer.name,
+      ShipToCustomerName2: this.shipToCustomer.name2,
+      CustomerPriceProcdure: this.customerPriceProcedure,
+      MaterialDisplay: this.selectedDescription,
+      PaymentTerms: this.selectedCustomer.termsofpayment,
+      Incoterm: this.selectedCustomer.incoterms,
+      IncotermPort: this.selectedCustomer.incotermspart2,
+      TradingCurrencyId: this.selectedCustomer.currency_id,
+      ValidFrom: this.validFromDate,
+      ValidTo: this.validToDate,
+      deliveryDate: this.deliveryDate,
+      SalesEmployeeNumber: this.salesEmployee.employeeno,
+      DeliveryDate: this.deliveryDate,
+    }
+    this.http.post(`${environment.apiUrl}/save-quote`, quoteData).subscribe({
+      next: (response) => {
+        console.log('Quote saved successfully', response)
+      },
+      error: (error) => {
+        console.error('Error saving quote', error)
+      },
     })
   }
 }
